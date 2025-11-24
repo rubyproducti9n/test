86b87d789cdd4d82c8705207c79795259860b8f7
Om Lokhande <91490800+rubyproducti9n@users.noreply.github.com>
2025-11-24 14:47:49 +0530

    fix

--- src/pages/Admin/login.jsx
+++ src/pages/Admin/login.jsx
@@ -188,7 +188,7 @@ export default function AdminLogin() {
               </span>
             </p>
 
-            <button type="submit" className="login-btn" onClick={() => navigate("/Dashboard")}>Login</button>
+            <button type="submit" className="login-btn">Login</button>
           </form>
         </div>
       </div>
